528de8141009ddc9ad829b01625c19cdd682d68b
Matt <124936174+MattyPLovesLemons@users.noreply.github.com>
2023-03-21 23:59:23 -0400

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const input = document.querySelector("#Tinput");
 const tskList = document.querySelector("#tasks");
 
 let badges = [{ name:"Boulder", imgB:"/Images/badge/b1.png"}
-  ,{name:"Cascade", imgB:"./Images/badge/b2.png"}
+  ,{name:"Cascade", imgB:"Images/badge/b2.png"}
   ,{name:"Thunder", imgB:"./Images/badge/b3.png"}
   ,{name:"Rainbow", imgB:"./Images/badge/b4.png"}
   ,{name:"Soul", imgB:"./Images/badge/b5.png"}
